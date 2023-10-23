49275c76261c3a0467255b9fa7f251c28bbcd394
jedidia60 <jedidiahwnt@hotmail.com>
2023-10-23 09:57:00 +0800

    Updated code for 4a and add comments
    It is still incomplete, need to find how to get moveDir

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Mario/Mario.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Mario/Mario.cs
@@ -14,19 +14,32 @@ public class Mario : MonoBehaviour
 
     void Start()
     {
+        // Set the rb variable to the object's RigidBody2D
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        gravityDir = (planet.position - transform.position).normalized;
+        // Get the direction for where the gravitational force is to be applied on
+        gravityDir = (planet.position - transform.position);
+
+        // Set the direction to move the character based on the input given
         moveDir = new Vector3();
         moveDir = moveDir.normalized * -1f;
 
-        rb.AddForce(moveDir);
-        
-        //gravityNorm = 
-        rb.AddForce(-moveDir);
+        // Add force to the RigidBody2D to move the character in the specified direction and the speed specified in force
+        rb.AddForce(moveDir * force);
+
+        // Normalize the direction of the gravity
+        // Apply the gravitational force by adding force to the RigidBody2D with gravityStrength in the direction given by gravityNorm
+        gravityNorm = gravityDir.normalized;
+        rb.AddForce(gravityNorm * gravityStrength);
+
+        // Calculate the angle needed to rotate the character as they move around the planet
+        float angle = Vector3.SignedAngle(transform.position, moveDir, Vector3.forward);
+
+        // Slowly rotate the character by the angle calculated
+        rb.MoveRotation(Quaternion.Euler(angle, 0, 0));
     }
 }
 
