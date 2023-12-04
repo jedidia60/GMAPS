28ef5693a66c3889c4a18453da0b7f297e2cf439
jedidia60 <jedidiahwnt@hotmail.com>
2023-12-05 07:50:31 +0800

    Completed part 2

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Ball2D.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Ball2D.cs
@@ -1,53 +1,57 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Ball2D : MonoBehaviour
-// {
-//     public HVector2D Position = new HVector2D(0, 0);
-//     public HVector2D Velocity = new HVector2D(0, 0);
-    
-//     [HideInInspector]
-//     public float Radius;
+public class Ball2D : MonoBehaviour
+{
+    public HVector2D Position = new HVector2D(0, 0);
+    public HVector2D Velocity = new HVector2D(0, 0);
 
-//     private void Start()
-//     {
-//         Position.x = transform.position.x;
-//         Position.y = transform.position.y;
+    [HideInInspector]
+    public float Radius;
 
-//         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-//         Vector2 sprite_size = sprite.rect.size;
-//         Vector2 local_sprite_size = sprite_size / sprite.pixelsPerUnit;
-//         Radius = local_sprite_size.x / 2f;
-//     }
+    private void Start()
+    {
+        Position.x = transform.position.x;
+        Position.y = transform.position.y;
 
-//     public bool IsCollidingWith(float x, float y)
-//     {
-//         float distance = /*your code here*/;
-//         return distance <= Radius;
-//     }
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Vector2 sprite_size = sprite.rect.size;
+        Vector2 local_sprite_size = sprite_size / sprite.pixelsPerUnit;
+        Radius = local_sprite_size.x / 2f;
 
-//     public bool IsCollidingWith(Ball2D other)
-//     {
-//         float distance = Util.FindDistance(Position, other.Position);
-//         return distance <= Radius + other.Radius;
-//     }
+        HVector2D a = new HVector2D(8f, 5f);
+        HVector2D b = new HVector2D(1f, 3f);
+        float distance = Util.FindDistance(a, b);
+    }
 
-//     public void FixedUpdate()
-//     {
-//         UpdateBall2DPhysics(Time.deltaTime);
-//     }
+    public bool IsCollidingWith(float x, float y)
+    {
+        float distance = Util.FindDistance(Position, new HVector2D(x, y));
+        return distance <= Radius;
+    }
 
-//     private void UpdateBall2DPhysics(float deltaTime)
-//     {
-//         float displacementX = /*your code here*/;
-//         float displacementY = /*your code here*/;
+    //public bool IsCollidingWith(Ball2D other)
+    //{
+    //    float distance = Util.FindDistance(Position, other.Position);
+    //    return distance <= Radius + other.Radius;
+    //}
 
-//         Position.x += /*your code here*/;
-//         Position.y += /*your code here*/;
+    public void FixedUpdate()
+    {
+        UpdateBall2DPhysics(Time.deltaTime);
+    }
 
-//         transform.position = new Vector2(/*your code here*/);
-//     }
-// }
+    private void UpdateBall2DPhysics(float deltaTime)
+    {
+        float displacementX = Velocity.x * deltaTime;
+        float displacementY = Velocity.y * deltaTime;
+
+        Position.x += displacementX;
+        Position.y += displacementY;
+
+        transform.position = new Vector2(Position.x, Position.y);
+    }
+}
 
--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -1,12 +1,12 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Util
-// {
-//     public static float FindDistance(HVector2D p1, HVector2D p2)
-//     {
-//         return /*your code here*/;
-//     }
-// }
+public class Util
+{
+    public static float FindDistance(HVector2D p1, HVector2D p2)
+    {
+        return Mathf.Sqrt(Mathf.Pow(p1.x - p2.x,2) + Mathf.Pow(p1.y - p2.y, 2));
+    }
+}
 
